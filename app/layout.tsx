27118f15a032c081fc6907e1cef51136ef7924d4
爱铭网络 <308207109@users.noreply.github.com>
2024-05-03 23:33:19 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT - 爱铭网络",
+  description: "这是一个免费的 ChatGPT 机器人.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "ChatGPT - 爱铭网络",
     statusBarStyle: "default",
   },
 };
